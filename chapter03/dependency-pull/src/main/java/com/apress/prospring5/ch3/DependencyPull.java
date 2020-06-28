8577d93673acd5f311598997bcaca7fe0b4f5675
招文桃 <gdouzwt@gmail.com>
2020-06-28 11:56:19 +0800

    dependency-pull 加了阅读注释

--- chapter03/dependency-pull/src/main/java/com/apress/prospring5/ch3/DependencyPull.java
+++ chapter03/dependency-pull/src/main/java/com/apress/prospring5/ch3/DependencyPull.java
@@ -4,6 +4,9 @@ import com.apress.prospring5.ch2.decoupled.MessageRenderer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * 通过main方法初始启动 spring 容器的过程属于依赖查找，dependency-pull 的实现方式
+ */
 public class DependencyPull {
     public static void main(String... args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext
