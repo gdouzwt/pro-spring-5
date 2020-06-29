cf1b1f75fa61327dd033bd2c0c2b03bc49e23e30
招文桃 <zhao@wen-tao.com>
2020-06-30 01:11:49 +0800

    搞清楚了Idea设置的method-injection的3个Spring Application Context

--- chapter03/method-injection/src/main/java/com/apress/prospring5/ch3/AbstractLookupDemoBean.java
+++ chapter03/method-injection/src/main/java/com/apress/prospring5/ch3/AbstractLookupDemoBean.java
@@ -1,6 +1,10 @@
 package com.apress.prospring5.ch3;
 
+/**
+ * 不使用注解的情况，就用抽象类
+ */
 public abstract class AbstractLookupDemoBean implements DemoBean {
+    // lookup method，通过这个方法返回非单例bean
     public abstract Singer getMySinger();
 
     @Override
--- chapter03/method-injection/src/main/java/com/apress/prospring5/ch3/Singer.java
+++ chapter03/method-injection/src/main/java/com/apress/prospring5/ch3/Singer.java
@@ -2,6 +2,7 @@ package com.apress.prospring5.ch3;
 
 /**
  * Created by iuliana.cosmina on 2/20/17.
+ * 这是一个 prototype 类型的 bean
  */
 public class Singer {
     private String lyric = "I played a quick game of chess with the salt and pepper shaker";
