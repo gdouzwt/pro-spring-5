4384f275e42eb28c2e0edb1f0e8772a50cfb64f0
招文桃 <gdouzwt@gmail.com>
2020-06-30 12:37:34 +0800

    chapter04 application-context-aware 看完，生命周期钩子

--- chapter04/application-context-aware/src/main/java/com/apress/prospring5/ch4/ShutdownHookBean.java
+++ chapter04/application-context-aware/src/main/java/com/apress/prospring5/ch4/ShutdownHookBean.java
@@ -5,6 +5,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.GenericApplicationContext;
 
+import javax.annotation.PreDestroy;
+
+/**
+ * 这粒 Bean 通过实现 ApplicationContextAware 接口，可以感知当前容器环境（ApplicationContext）
+ * 其实这个例子的意思是，通过将某粒 Bean 定义为实现 ApplicationContextAware 以便给 ApplicationContext 注册
+ * 一个 ShutdownHook。 这样的话，被 @PreDestroy 之类标记的方法就会在容器关闭前被调用
+ */
 public class ShutdownHookBean implements ApplicationContextAware {
     private ApplicationContext ctx;
 
@@ -14,8 +21,17 @@ public class ShutdownHookBean implements ApplicationContextAware {
     public void setApplicationContext(ApplicationContext ctx)
         throws BeansException {
 
+        // 如果是 GenericApplicationContext 示例就注册一个容器关闭的钩子（registerShutdownHook）
         if (ctx instanceof GenericApplicationContext) {
             ((GenericApplicationContext) ctx).registerShutdownHook();
         }
+    }
+
+    /**
+     * 添加用于测试
+     */
+    @PreDestroy
+    public void close() {
+        System.out.println("Closing down...");
     }
 }
