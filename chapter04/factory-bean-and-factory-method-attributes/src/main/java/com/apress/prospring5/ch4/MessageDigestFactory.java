77f348c7493d183a8101866375cea84bb21d7ddf
招文桃 <gdouzwt@gmail.com>
2020-06-30 19:18:26 +0800

    chapter04/factory-bean-and-factory-method-attributes 搞完了

--- chapter04/factory-bean-and-factory-method-attributes/src/main/java/com/apress/prospring5/ch4/MessageDigestFactory.java
+++ chapter04/factory-bean-and-factory-method-attributes/src/main/java/com/apress/prospring5/ch4/MessageDigestFactory.java
@@ -2,9 +2,11 @@ package com.apress.prospring5.ch4;
 
 import java.security.MessageDigest;
 
+// factory-bean
 public class MessageDigestFactory {
     private String algorithmName = "MD5";
 
+    // factory-method
     public MessageDigest createInstance() throws Exception {
         return MessageDigest.getInstance(algorithmName);
     }
